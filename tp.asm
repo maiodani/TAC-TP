99e9316bfd842e292c60efdda75ea45ebe86e290
a2020127363 <a2020127363@isec.pt>
2021-06-03 20:07:57 +0100

    tempo, horas

--- tp.asm
+++ tp.asm
@@ -15,7 +15,7 @@ dseg	segment para public 'data'
 		Tempo_init		dw		0				; Guarda O Tempo de inicio do jogo
 		Tempo_j			dw		0				; Guarda O Tempo que decorre o  jogo
 		Tempo_limite	dw		100				; tempo m�ximo de Jogo
-		String_TJ		db		"    /100$"
+		String_TJ		db		"   /100$"
 
 		String_num 		db 		"  0 $"
         String_nome  	db	    "ISEC  $"	
@@ -42,6 +42,7 @@ dseg	segment para public 'data'
 		POSxa			db	3	; Posi��o anterior de x
 		paredecar		db  32
 		teclapress		db  0
+		
 dseg	ends
 
 cseg	segment para public 'code'
@@ -136,11 +137,129 @@ sai_f:
 		
 IMP_FICH	endp		
 
+;########################################################################
+; Imprime o tempo no monitor
+Ler_TEMPO PROC	
+ 
+		PUSH AX
+		PUSH BX
+		PUSH CX
+		PUSH DX
+	
+		PUSHF
+		
+		MOV AH, 2CH             ; Buscar a hORAS
+		INT 21H                 
+		
+		XOR AX,AX
+		MOV AL, DH              ; segundos para al
+		mov Segundos, AX		; guarda segundos na variavel correspondente
+		
+		XOR AX,AX
+		MOV AL, CL              ; Minutos para al
+		mov Minutos, AX         ; guarda MINUTOS na variavel correspondente
+		
+		XOR AX,AX
+		MOV AL, CH              ; Horas para al
+		mov Horas,AX			; guarda HORAS na variavel correspondente
+ 
+		POPF
+		POP DX
+		POP CX
+		POP BX
+		POP AX
+ 		RET 
+Ler_TEMPO   ENDP 
 
+Trata_Horas PROC
+
+		PUSHF
+		PUSH AX
+		PUSH BX
+		PUSH CX
+		PUSH DX		
+
+		CALL 	Ler_TEMPO				; Horas MINUTOS e segundos do Sistema
+		
+		MOV		AX, Segundos
+		cmp		AX, Old_seg			; VErifica se os segundos mudaram desde a ultima leitura
+		je		fim_horas			; Se a hora não mudou desde a última leitura sai.
+		mov		Old_seg, AX			; Se segundos são diferentes actualiza informação do tempo 
+		
+		mov 	ax,Horas
+		MOV		bl, 10     
+		div 	bl
+		add 	al, 30h				; Caracter Correspondente às dezenas
+		add		ah,	30h				; Caracter Correspondente às unidades
+		MOV 	STR12[0],al			; 
+		MOV 	STR12[1],ah
+		MOV 	STR12[2],'h'		
+		MOV 	STR12[3],'$'
+		goto_xy 2,0
+		MOSTRA STR12 		
+        
+		mov 	ax,Minutos
+		MOV 	bl, 10     
+		div 	bl
+		add 	al, 30h				; Caracter Correspondente às dezenas
+		add		ah,	30h				; Caracter Correspondente às unidades
+		MOV 	STR12[0],al			; 
+		MOV 	STR12[1],ah
+		MOV 	STR12[2],'m'		
+		MOV 	STR12[3],'$'
+		goto_xy	6,0
+		MOSTRA	STR12 		
+		
+		mov 	ax,Segundos
+		MOV 	bl, 10     
+		div 	bl
+		add 	al, 30h				; Caracter Correspondente às dezenas
+		add		ah,	30h				; Caracter Correspondente às unidades
+		MOV 	STR12[0],al			; 
+		MOV 	STR12[1],ah
+		MOV 	STR12[2],'s'		
+		MOV 	STR12[3],'$'
+		goto_xy	10,0
+		MOSTRA	STR12 	
+		mov cx,100
+
+		inc		Tempo_j
+		mov 	ax,Tempo_j
+		MOV 	bl, 10     
+		div 	bl
+		
+		add 	al, 30h				; Caracter Correspondente às dezenas
+		add		ah,	30h				; Caracter Correspondente às unidades
+		
+		MOV 	String_TJ[1],al
+		MOV		String_TJ[2],ah
+		goto_xy	60,0
+		MOSTRA	String_TJ	
+
+						
+fim_horas:		
+		goto_xy	POSx,POSy			; Volta a colocar o cursor onde estava antes de actualizar as horas
+		
+		POPF
+		POP DX		
+		POP CX
+		POP BX
+		POP AX
+		RET		
+			
+Trata_Horas ENDP
+Tempo PROC
+
+Tempo ENDP
 ;########################################################################
 ; LE UMA TECLA	
-
 LE_TECLA	PROC
+sem_tecla:
+		call 	Trata_Horas
+		MOV		AH,0BH
+		INT 	21h
+		cmp 	AL,0
+		je	    sem_tecla
 		
 		mov		ah,08h
 		int		21h
@@ -168,15 +287,15 @@ AVATAR	PROC
 			int		10h			
 			mov		Car, al			; Guarda o Caracter que est� na posi��o do Cursor
 			mov		Cor, ah			; Guarda a cor que est� na posi��o do Cursor	
-	
 
-CICLO:		goto_xy	1,1
+			goto_xy	1,1
 			mov 	ah, 08h			; Guarda o Caracter que est� na posi��o do Cursor
 			mov		bh,0			; numero da p�gina
 			int		10h	
 			mov paredecar,al
-			
-			goto_xy	POSx,POSy		; Vai para nova possi��o
+	
+
+CICLO:		goto_xy	POSx,POSy		; Vai para nova possi��o
 			mov 	ah, 08h
 			mov		bh,0			; numero da p�gina
 			int		10h	
@@ -267,7 +386,8 @@ fim:
 			RET
 AVATAR		endp
 
-
+;########################################################################
+; MAIN
 Main  proc
 		mov			ax, dseg
 		mov			ds,ax
@@ -275,7 +395,6 @@ Main  proc
 		mov			ax,0B800h
 		mov			es,ax
 		call		apaga_ecran
-		
 		goto_xy		0,0
 		call		IMP_FICH
 		call 		AVATAR
